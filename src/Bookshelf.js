cc78bc5eb08d43044c4d65a978331f58297eebed
Ken Jannette <KenJannette@Kens-MBP.attlocal.net>
2018-04-17 20:56:29 -0400

    No message

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -21,7 +21,7 @@ class Bookshelf extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li> // ____________________________________________________
+                  <li> 
                     <div className="book">
                       <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
@@ -38,7 +38,7 @@ class Bookshelf extends Component {
                       <div className="book-title">To Kill a Mockingbird</div>
                       <div className="book-authors">Harper Lee</div>
                     </div>
-                  </li> // ____________________________________________________
+                  </li>
                   <li>
                     <div className="book">
                       <div className="book-top">
