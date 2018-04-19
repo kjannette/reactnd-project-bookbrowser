f260cc13f20bd559648a85829fa41a3eda026e1b
Ken Jannette <KenJannette@Kens-MBP.attlocal.net>
2018-04-19 15:06:35 -0400

    No message

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class BooksApp extends React.Component {
   onSelectChange = (e) => {
     const id = e.target.name
     const newShelf = e.target.value
-    const books = this.state.books.slice();
+    const books = this.state.books
     books.forEach((book) => {
       if (book.id === id) {
         book.shelf = newShelf;
