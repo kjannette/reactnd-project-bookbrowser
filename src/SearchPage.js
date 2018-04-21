e7b57c8f98a88306d92f93948391f5bade07fffc
Ken Jannette <KenJannette@Kens-MBP.attlocal.net>
2018-04-20 20:01:22 -0400

    Bug fix for SearchPage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -42,7 +42,7 @@ class SearchPage extends Component {
                 return <li key={result.id}>
                           <div className="book">
                               <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ result.imageLinks.thumbnail }")` }}></div>
+                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ result.imageLinks.thumbnail }")`  }}></div>
                                       <div className="book-shelf-changer">
                                         <select name={result.id} onChange={onSelect}>
                                           <option value="none">Move to...</option>
