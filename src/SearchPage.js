6dca84899f9f044d0e48499ffc06447fdd642db4
Ken Jannette <KenJannette@Kens-MBP.attlocal.net>
2018-04-21 14:39:34 -0400

    added logic to compare search results to books on shelves in order to set correct shelf for search result books

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -12,8 +12,17 @@ class SearchPage extends Component {
   }
 
   onSearch = (query) => {
+    const books = this.props.books
+  //  console.log(books)
     if (query.length > 3) {
       search(query).then((results) => {
+        for (result in results) {
+          for (book in books) {
+            if (result === book) {
+              result.shelf = book.shelf
+            }
+          }
+        }
         this.setState({ results });
       })
     }
